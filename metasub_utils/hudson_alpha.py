794239e035fb4cd189ed928a8231a6503da2da28
Danko David <dcd3001@pascal.med.cornell.edu>
2018-08-28 11:55:59 -0400

    add sl table

--- metasub_utils/hudson_alpha.py
+++ metasub_utils/hudson_alpha.py
@@ -23,6 +23,7 @@ from os import makedirs
 from os.path import join, basename, dirname, isfile
 from concurrent.futures import ThreadPoolExecutor
 import requests
+from sys import stderr
 
 
 ###############################################################################
@@ -49,16 +50,35 @@ def rename_sl_names_to_ha_unique():
 def map_from_name_in_datasuper_to_ha_unique(source_fastqs_file):
     name_map = {}
     for line in source_fastqs_file:
+        if len(line.strip()) == 0:
+            continue
         tkns = line.split()
         if '1.fastq' not in tkns[0]:
             continue
         ds_name = basename(tkns[0]).split('_1.')[0].split('.R1.')[0]
-        ha_unique = basename(rename_one_sl_path_to_ha_unique(tkns[1]))
+        try:
+            ha_unique = basename(rename_one_sl_path_to_ha_unique(tkns[1])).split('_1.fastq.gz')[0]
+        except IndexError:
+            print('!!! ' + line, file=stderr)
         assert ds_name not in name_map
         name_map[ds_name] = ha_unique
     return name_map
 
 
+def rename_existing_core_results(source_fastqs_file, old_result_dir):
+    name_map = map_from_name_in_datasuper_to_ha_unique(source_fastqs_file)
+    path_map = {}
+    for filepath in glob(old_result_dir + '/*/*'):
+        old_name = basename(filepath).split('.')[0]
+        try:
+            new_name = name_map[old_name]
+        except KeyError:
+            print(f'!!! {old_name}', file=stderr)
+            continue
+        new_path = 'new_metasub_cap'.join(filepath.split('metasub_cap'))
+        new_path = new_name.join(new_path.split(old_name))
+        path_map[filepath] = new_path
+    return path_map
 
 
 ###############################################################################
