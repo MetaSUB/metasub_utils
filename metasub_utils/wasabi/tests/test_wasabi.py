70b550fe2e7714aa1ce208e1dfbed40333e7796a
David Danko <dcdanko@gmail.com>
2018-12-23 16:27:22 -0500

    added circleci config and one test suite

---
+++ metasub_utils/wasabi/tests/test_wasabi.py
@@ -0,0 +1,18 @@
+"""Test suite for wasabi."""
+
+from unittest import TestCase
+from os.path import isfile
+from random import randint
+
+from metasub_utils.wasabi import WasabiBucket
+
+
+class TestWasabi(TestCase):
+    """Test suite for wasabi."""
+
+    def test_download_file(self):
+        bucket = WasabiBucket()
+        local_name = f'temp_{randint(0, 1000 * 1000)}'
+        bucket.download('metasub/Scripts/downloadem.sh', local_name, False)
+        self.assertTrue(isfile(local_name))
+        self.assertTrue(len(open(local_name).read()) > 0)
