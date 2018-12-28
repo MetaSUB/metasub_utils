e5210c479e6787fc94cd30216a26c24c297b92e1
David Danko <dcdanko@gmail.com>
2018-12-28 11:17:44 -0500

    use aws credentials in test

--- metasub_utils/wasabi/tests/test_wasabi.py
+++ metasub_utils/wasabi/tests/test_wasabi.py
@@ -1,16 +1,30 @@
 """Test suite for wasabi."""
 
 from unittest import TestCase
-from os import getcwd
-from os.path import isfile
+from os import getcwd, mkdir, environ
+from os.path import isfile, dirname
 from random import randint
 
 from metasub_utils.wasabi import WasabiBucket
 
 
+def with_aws_credentials(func):
+    """Make a default AWS credential file in the home dir."""
+    cred_filename = environ['HOME'] + '/.aws/credentials'
+    mkdir(dirname(cred_filename), exist_ok=True)
+
+    access_key, secret_key = environ['AWS_ACCESS_KEY'], environ['AWS_SECRET_ACCESS_KEY']
+    cred_str = f'[default]\naws_access_key_id={access_key}\naws_secret_access_key={secret_key}\n'
+    with open(cred_filename, 'w') as cf:
+        cf.write(cred_str)
+
+    return func
+
+
 class TestWasabi(TestCase):
     """Test suite for wasabi."""
 
+    @with_aws_credentials
     def test_download_file(self):
         bucket = WasabiBucket()
         local_name = f'{getcwd()}/temp_{randint(0, 1000 * 1000)}'
