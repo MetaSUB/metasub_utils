8ce02d79cb4992ffc0a4a7f4bf66ed860d2299aa
David Danko <dcdanko@gmail.com>
2019-01-04 12:29:58 -0500

    bump minor version of overall project

--- metasub_utils/wasabi/setup.py
+++ metasub_utils/wasabi/setup.py
@@ -11,7 +11,7 @@ requirements = [
 
 setup(
     name=microlib_name,
-    version='0.3.0',
+    version='0.4.0',
     author='David Danko',
     author_email='dcdanko@gmail.com',
     license='MIT license',
