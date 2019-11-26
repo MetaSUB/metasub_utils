b445c79b2f857352f9705aafc3c024817166f533
David Danko <dcdanko@gmail.com>
2019-11-26 11:38:08 -0500

    include urls in package

--- metasub_utils/hudson_alpha/setup.py
+++ metasub_utils/hudson_alpha/setup.py
@@ -8,7 +8,7 @@ requirements = [
 
 setup(
     name=microlib_name,
-    version='0.3.0',
+    version='0.3.1',
     author='David Danko',
     author_email='dcdanko@gmail.com',
     license='MIT license',
@@ -20,4 +20,7 @@ setup(
     namespace_packages=['metasub_utils'],
     packages=[microlib_name],
     install_requires=requirements,
+    package_data={microlib_name: [
+        'hudson_alpha_flowcells.csv',
+    ]},
 )
